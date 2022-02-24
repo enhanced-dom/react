bf71c388728048a7c14c43ef8f5a259283ec61c5
Enhanced Dom <enhanced.dom@gmail.com>
2022-02-24 21:18:39 +0100

    taking advantage of component self-registration

--- src/webcomponent.adapter.ts
+++ src/webcomponent.adapter.ts
@@ -2,11 +2,17 @@ import omit from 'lodash.omit'
 import React, { createElement, forwardRef, useEffect, useRef, useCallback } from 'react'
 import isPlainObject from 'lodash.isplainobject'
 
-interface WebcomponentPrototype<WebcomponentElement extends HTMLElement, ConstructorArgsTypes extends any[] = any[]> {
-  new (...args: ConstructorArgsTypes): WebcomponentElement
+type WebcomponentPrototype<WebcomponentElement extends HTMLElement, ConstructorArgsTypes extends any[] = any[]> = {
+  new(...args: ConstructorArgsTypes): WebcomponentElement
   renderer?: any
   [key: string]: any
-}
+} & ({
+  register: () => void
+  tag: string
+} | {
+  register?: never
+  tag?: never
+})
 
 type EventMapping = Record<string, string> | ((fromPropName: string) => string | null)
 
@@ -25,8 +31,19 @@ type CopiedStatics<
   WebcomponentElement extends HTMLElement,
   S extends WebcomponentPrototype<WebcomponentElement>,
   StaticsType extends string,
-> = {
-  [key in StaticsType]: S[key]
+  > = {
+    [key in StaticsType]: S[key]
+  }
+
+const ensureElementIsRegistered = <WebcomponentElement extends HTMLElement, ConstructorArgsTypes extends any[],
+  WebcomponentType extends WebcomponentPrototype<WebcomponentElement, ConstructorArgsTypes>>(tag: string, type?: WebcomponentType) => {
+  if (type) {
+    if (type.register) {
+      type.register()
+    } else if (!window.customElements.get(tag)) {
+      window.customElements.define(tag, type)
+    }
+  }
 }
 
 export function withReactAdapter<
@@ -37,91 +54,94 @@ export function withReactAdapter<
   ModifiedAttributesType = Record<string, string>,
   StaticsType extends string = 'renderer',
   ExtraStatics extends Record<string, any> = Record<string, AnalyserNode>,
->({
-  type,
-  name,
-  hoistedProps = ['renderer'] as StaticsType[],
-  eventMapping = withReactAdapter.defaultEventMapping,
-  propsTransformer = withReactAdapter.defaultPropsTransformer,
-}: {
-  type?: WebcomponentType
-  hoistedProps?: StaticsType[]
-  name: string
-  eventMapping?: EventMapping
-  propsTransformer?: (props: Partial<ReactAttributesType>) => ModifiedAttributesType
-}) {
-  if (type) {
-    window.customElements.define(name, type)
-  }
-  const WebcomponentWrapper = (props: ReactAttributesType, ref?: React.Ref<any>) => {
-    const webComponentRef = useRef<any>()
-    const isEvent = isEventEvaluator(eventMapping)
-    const mapEvent = eventMapper(eventMapping)
+  >({
+    type,
+    tag,
+    hoistedProps = ['renderer'] as StaticsType[],
+    eventMapping = withReactAdapter.defaultEventMapping,
+    propsTransformer = withReactAdapter.defaultPropsTransformer,
+  }: {
+    hoistedProps?: StaticsType[]
+    eventMapping?: EventMapping
+    propsTransformer?: (props: Partial<ReactAttributesType>) => ModifiedAttributesType
+  } & ({
+    type: WebcomponentType
+    tag?: string
+  } | {
+    type?: WebcomponentType
+    tag: string 
+  })) {
+    const elementTag = type?.tag ?? tag
+    const WebcomponentWrapper = (props: ReactAttributesType, ref?: React.Ref<any>) => {
+      ensureElementIsRegistered(elementTag, type)
+      const webComponentRef = useRef<any>()
+      const isEvent = isEventEvaluator(eventMapping)
+      const mapEvent = eventMapper(eventMapping)
 
-    const eventProps = Object.keys(props).filter((propName) => isEvent(propName))
-    const forwardProps = omit(props, eventProps)
+      const eventProps = Object.keys(props).filter((propName) => isEvent(propName))
+      const forwardProps = omit(props, eventProps)
 
-    const callbackRef = useCallback(
-      (node) => {
-        if (node !== null) {
-          webComponentRef.current = node
-          if (ref) {
-            if (typeof ref === 'function') {
-              ref(node)
-            } else {
-              ;(ref as any).current = node
+      const callbackRef = useCallback(
+        (node) => {
+          if (node !== null) {
+            webComponentRef.current = node
+            if (ref) {
+              if (typeof ref === 'function') {
+                ref(node)
+              } else {
+                ; (ref as any).current = node
+              }
             }
+
+            eventProps.forEach((propName) => {
+              webComponentRef.current.addEventListener(mapEvent(propName), props[propName])
+            })
           }
+        },
+        [eventProps, mapEvent, props, ref, webComponentRef],
+      )
 
-          eventProps.forEach((propName) => {
-            webComponentRef.current.addEventListener(mapEvent(propName), props[propName])
-          })
+      useEffect(() => {
+        return () => {
+          if (webComponentRef.current) {
+            eventProps.forEach((propName) => {
+              webComponentRef.current.removeEventListener(mapEvent(propName), props[propName])
+            })
+          }
         }
-      },
-      [eventProps, mapEvent, props, ref, webComponentRef],
-    )
+      }, [webComponentRef, eventProps, mapEvent, props])
 
-    useEffect(() => {
-      return () => {
-        if (webComponentRef.current) {
-          eventProps.forEach((propName) => {
-            webComponentRef.current.removeEventListener(mapEvent(propName), props[propName])
-          })
-        }
-      }
-    }, [webComponentRef, eventProps, mapEvent, props])
+      return createElement(elementTag, {
+        ref: callbackRef,
+        ...forwardProps,
+        ...propsTransformer(forwardProps),
+      })
+    }
 
-    return createElement(name, {
-      ref: callbackRef,
-      ...forwardProps,
-      ...propsTransformer(forwardProps),
-    })
+    WebcomponentWrapper.displayName = `withReactAdapter<${elementTag}>`
+
+    const compWithForwardedRef = forwardRef(WebcomponentWrapper) as unknown as React.ComponentType<ReactAttributesType> &
+      CopiedStatics<WebcomponentElement, WebcomponentType, StaticsType> &
+      ExtraStatics
+    if (type) {
+      hoistedProps.forEach((hoistedProp) => {
+        Object.defineProperty(compWithForwardedRef, hoistedProp, { value: type[hoistedProp], writable: false })
+      })
+    }
+    return compWithForwardedRef
   }
 
-  WebcomponentWrapper.displayName = `withReactAdapter<${name}>`
-
-  const compWithForwardedRef = forwardRef(WebcomponentWrapper) as unknown as React.ComponentType<ReactAttributesType> &
-    CopiedStatics<WebcomponentElement, WebcomponentType, StaticsType> &
-    ExtraStatics
-  if (type) {
-    hoistedProps.forEach((hoistedProp) => {
-      Object.defineProperty(compWithForwardedRef, hoistedProp, { value: type[hoistedProp], writable: false })
-    })
-  }
-  return compWithForwardedRef
-}
-
-withReactAdapter.defaultEventMapping = (fromPropName: string) =>
-  /on[A-Z]+/.test(fromPropName) ? fromPropName.substring(2).toLowerCase() : null
-withReactAdapter.defaultPropsTransformer = <
-  ReactAttributesType extends { className?: string; style?: React.CSSProperties; children?: React.ReactNode },
-  ModifiedAttributesType = Record<string, string>,
->({
-  style,
-  className,
-  children,
-  ...rest
-}: ReactAttributesType) =>
+  withReactAdapter.defaultEventMapping = (fromPropName: string) =>
+    /on[A-Z]+/.test(fromPropName) ? fromPropName.substring(2).toLowerCase() : null
+  withReactAdapter.defaultPropsTransformer = <
+    ReactAttributesType extends { className?: string; style?: React.CSSProperties; children?: React.ReactNode },
+    ModifiedAttributesType = Record<string, string>,
+    >({
+      style,
+      className,
+      children,
+      ...rest
+    }: ReactAttributesType) =>
   ({
     className,
     style,
@@ -130,4 +150,5 @@ withReactAdapter.defaultPropsTransformer = <
       (seed, acc) => ({ ...seed, [acc]: Array.isArray(rest[acc]) || isPlainObject(rest[acc]) ? JSON.stringify(rest[acc]) : rest[acc] }),
       {} as Record<keyof ReactAttributesType, string>,
     ),
-  } as unknown as ModifiedAttributesType)
+  } as unknown as ModifiedAttributesType
+)
